42f05986dd52af251670e8f06bbd38211a711f09
Carla <carladmaximo@gmail.com>
2021-07-14 20:05:25 -0300

    Adicionando projetos Trybe

---
+++ pixelArt/script.js
@@ -0,0 +1,103 @@
+// Creating pixels grid by JS
+// Requirement 4
+const pixelBoard = document.getElementById('pixel-board');
+function pixelGrid(input) {
+  for (let rows = 0; rows < input; rows += 1) {
+    const row = document.createElement('section');
+
+    for (let columns = 0; columns < input; columns += 1) {
+      const pixels = document.createElement('div');
+
+      pixels.classList.add('pixel');
+      row.appendChild(pixels);
+    }
+    pixelBoard.appendChild(row);
+  }
+}
+pixelGrid(5);
+// Requirement 06
+document.getElementById('black').classList.add('selected');
+
+// Requirement 07
+function selectedColor(currentColor) {
+  const lastColorSelected = document.querySelector('.selected');
+  lastColorSelected.classList.remove('selected');
+  currentColor.target.classList.add('selected');
+}
+const colorPallete = document.getElementById('color-palette');
+colorPallete.addEventListener('click', selectedColor);
+
+// Requirement 08 - Thanks to Filipe Brochier for your help!
+function paint(event) {
+  event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+}
+pixelBoard.addEventListener('click', paint);
+
+// Requirement 09
+const btnClear = document.getElementById('clear-board');
+btnClear.innerText = 'Limpar';
+
+function clearBoard() {
+  const square = document.querySelectorAll('.pixel');
+  for (let i = 0; i < square.length; i += 1) {
+    square[i].style.backgroundColor = null;
+  }
+}
+
+btnClear.addEventListener('click', clearBoard);
+
+// Requirement 09
+const BoardBtn = document.getElementById('generate-board');
+const inputBoard = document.getElementById('board-size');
+
+function sizeMinMax(input) {
+  if (input < 5 && input > 0) {
+    input = 5;
+    alert('O tamanho mínimo é 5.');
+  } else
+  if (input > 50) {
+    input = 50;
+    alert('O tamanho máximo é 50.');
+  } else {
+    return input;
+  }
+  // if (input >= 5 && input <= 50) {
+
+  // }
+  return input;
+}
+
+function btnVQV() {
+  const inputValue = inputBoard.value;
+  if (inputBoard.value === '') {
+    window.alert('Board inválido!');
+  }
+  const newBoard = sizeMinMax(inputValue);
+  pixelBoard.innerHTML = '';
+  return pixelGrid(newBoard);
+}
+
+BoardBtn.addEventListener('click', btnVQV);
+
+// Requirement 12
+// From: https://www.ti-enxame.com/pt/javascript/gerador-de-cores-aleatorias/967183954/
+function generateColor() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  const rgb = `rgb(${red}, ${green}, ${blue})`;
+  return rgb;
+}
+
+function changeColors() {
+  const colors = document.getElementsByClassName('color');
+
+  for (let i = 0; i < colors.length; i += 1) {
+    const color = colors[i];
+    if (color !== colors[0]) {
+      color.style.backgroundColor = generateColor();
+    }
+  }
+}
+changeColors();
---
+++ todoList/script.js
@@ -0,0 +1,125 @@
+// Requirement 05
+const inputText = document.getElementById('texto-tarefa');
+const btnAddText = document.querySelector('#criar-tarefa');
+const listText = document.getElementById('lista-tarefas');
+
+function addText() {
+  const inputValue = inputText.value;
+  const listItem = document.createElement('li');
+  listItem.innerText = inputValue;
+  listItem.classList.add('item-lista-tarefas');
+  listText.appendChild(listItem);
+  inputText.value = '';
+}
+btnAddText.addEventListener('click', addText);
+
+// Requirements 07, 08
+function changeColor(listText) {
+  const arrayLi = document.getElementsByTagName('li');
+  for (let i = 0; i < arrayLi.length; i += 1) {
+    arrayLi[i].classList.remove('selected');
+    // arrayLi[i].style.backgroundColor = 'white';
+    // listText.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    listText.target.classList.add('selected');
+  }
+}
+
+listText.addEventListener('click', changeColor);
+
+// Requirement 09
+function addLineThrough(event) {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
+listText.addEventListener('dblclick', addLineThrough);
+
+// Requirement 10
+const btnClear = document.getElementById('apaga-tudo');
+function btnClearList() {
+  listText.innerHTML = '';
+}
+
+btnClear.addEventListener('click', btnClearList);
+
+// Requirement 11
+// Sourched in https://pt.stackoverflow.com/questions/4605/remover-elemento-da-p%C3%A1gina-com-javascript
+const btnRemoveFinished = document.getElementById('remover-finalizados');
+const itensCompleted = document.getElementsByClassName('completed');
+function clearFinished() {
+  while (itensCompleted.length > 0) {
+    itensCompleted[0].parentNode.removeChild(itensCompleted[0]);
+  }
+}
+
+btnRemoveFinished.addEventListener('click', clearFinished);
+
+// Requirement 12
+const btnSave = document.getElementById('salvar-tarefas');
+function dataSave() { // Função que salva o conteúdo da "li" na chave "save"
+  const lis = document.querySelector('ol').innerHTML;
+  localStorage.setItem('save', lis); // Passa o conteúdo HTML de "lis" para a chave "save"
+}
+function load() {
+  if (localStorage.save !== undefined) { // se na chave "save" do localStorage não estiver "indefinido", ou seja, se não estiver vazia
+    document.querySelector('ol').innerHTML = localStorage.save; // o conteúdo HTML de toda a OL (e suas filhas) recebe o valor da chave "save" de localStorage. Isso garante que o conteúdo salvo em localStorage seja mostrado no navegador
+  }
+}
+window.onload = load(); // Função será executada quando o navegador carregar o site
+btnSave.addEventListener('click', dataSave);
+
+// Requirement 13
+// Referência: https://github.com/tryber/sd-014-a-project-todo-list/blob/mauricio-leite-todo-list-project/script.js
+// Agradeço ao Maurício Leite da turma 14, tribo B, por me permitir usar a mesma lógica que ele usou nessa parte do código dele!
+const btnUp = document.getElementById('mover-cima');
+const btndown = document.getElementById('mover-baixo');
+
+function moveUp() {
+  if (document.querySelectorAll('.selected').length > 0) {
+    const selected = document.querySelector('.selected').innerText;
+    const selectedClass = document.querySelector('.selected').className;
+    const prevClass = document.querySelector('.selected').previousSibling.className;
+    const prevItem = document.querySelector('.selected').previousSibling.innerText;
+
+    if (prevItem !== undefined) {
+      document.querySelector('.selected').previousSibling.innerText = selected;
+      document.querySelector('.selected').innerText = prevItem;
+      document.querySelector('.selected').previousSibling.className = selectedClass;
+      document.querySelector('.selected').nextSibling.className = prevClass;
+    }
+  }
+}
+
+function moveDown() {
+  if (document.querySelectorAll('.selected').length > 0) {
+    const selected = document.querySelector('.selected').innerText;
+    const selectedClass = document.querySelector('.selected').className;
+    const nextItem = document.querySelector('.selected').nextSibling;
+
+    if (nextItem !== null) {
+      const nextClass = document.querySelector('.selected').nextSibling.className;
+      document.querySelector('.selected').innerText = nextItem.innerText;
+      document.querySelector('.selected').nextSibling.className = selectedClass;
+
+      document.querySelector('.selected').nextSibling.innerText = selected;
+      document.querySelector('.selected').className = nextClass;
+    }
+  }
+}
+
+btnUp.addEventListener('click', moveUp);
+btndown.addEventListener('click', moveDown);
+
+// Requirement 14
+// Rererence: https://pt.stackoverflow.com/questions/4605/remover-elemento-da-p%C3%A1gina-com-javascript
+const btnRmSelected = document.getElementById('remover-selecionado');
+
+function removeSelected() {
+  if (document.querySelectorAll('.selected').length > 0) {
+    listText.removeChild(document.querySelector('.selected'));
+  }
+}
+
+btnRmSelected.addEventListener('click', removeSelected);
