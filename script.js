1ebbdad45f691dc5d40867bb7cd3e5c4b5cb19ea
Carla <carladmaximo@gmail.com>
2021-07-14 21:43:53 -0300

    Iniciando javascript nesse projeto

---
+++ script.js
@@ -0,0 +1,16 @@
+// Habilidades utilizadas
+const btnPixel = document.querySelector('#btn-pixel');
+
+function listaHabilidadesPixel() {
+  alert('Habilidades utilizadas: HTML5, CSS e JavaScript.');
+}
+
+btnPixel.addEventListener('click', listaHabilidadesPixel);
+
+const btnTarefas = document.querySelector('#btn-tarefas');
+
+function listaHabilidadesTarefas() {
+  alert('Habilidades utilizadas: HTML5, CSS e JavaScript.');
+}
+
+btnTarefas.addEventListener('click', listaHabilidadesTarefas);
